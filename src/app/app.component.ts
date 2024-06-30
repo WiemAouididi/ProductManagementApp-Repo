5c923e4e1fc8ef88fb59004c328b5c75d25dcb4b
WiemAouididi <wiemaouididi@gmail.com>
2024-06-30 04:47:48 +0200

    add and style product-list component in app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from "./shared/toolbar/toolbar.component";
+import { ProductListComponent } from "./components/product-list/product-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [
+    RouterOutlet,
+    ToolbarComponent,
+    ProductListComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
