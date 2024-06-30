e7356bd82e8a4eb786f3878db13f34cf7bee59b0
WiemAouididi <wiemaouididi@gmail.com>
2024-06-30 04:44:08 +0200

    list products and style product list page

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from "../../models/product.model";
+import { ProductService } from "../../services/product.service";
+import { MatCard, MatCardActions, MatCardContent } from "@angular/material/card";
+import { CurrencyPipe } from "@angular/common";
+import { MatIcon } from "@angular/material/icon";
+import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardActions,
+    CurrencyPipe,
+    MatCardContent,
+    MatIcon
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
+  products: Product[] = [];
+
+  constructor(private productService: ProductService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getProducts()
+      .subscribe(products => this.products = products);
+  }
 }
