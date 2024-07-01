342b7e62481e90cb10a351747086588d18d7f8a8
WiemAouididi <wiemaouididi@gmail.com>
2024-07-01 11:01:43 +0200

    add app rounting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {ProductListComponent} from "./components/product-list/product-list.component";
+import {ProductDetailComponent} from "./components/product-details/product-detail.component";
+import {ShoppingCartComponent} from "./components/shopping-cart/shopping-cart.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'cart', component: ShoppingCartComponent },
+];
